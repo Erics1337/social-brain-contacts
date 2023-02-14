aa73a810e7aa426a34fe364abe1c2de91b460688
Eric <erics1337@gmail.com>
2023-02-13 22:53:35 -0700

    rm stylesheet

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -73,17 +73,3 @@ function Home() {
 }
 
 export default Home;
-
-const styles = StyleSheet.create({
-  cardShadow: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-
-    elevation: 2,
-  },
-});
