383275f70163c3d338b8e55c7445f6784b4ce75e
Eric <erics1337@gmail.com>
2023-06-13 17:43:58 -0400

    ContactCard

--- components/ContactCard.jsx
+++ components/ContactCard.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import tw from 'tailwind-rn';
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import tw from 'tailwind-rn'
+import Communications from 'react-native-communications'
 
 const ContactCard = ({ contact }) => {
-  const { firstName, lastName, jobTitle, company, phoneNumbers, emails } = contact;
+	const { firstName, lastName, jobTitle, company, phoneNumbers, emails } =
+		contact
 
-  return (
-    <View style={tw(`bg-white rounded-lg p-4 shadow-md`)}>
-      <View style={tw(`flex-row items-center mb-4`)}>
-        <Text style={tw(`text-2xl font-bold mr-2`)}>{firstName}</Text>
-        <Text style={tw(`text-2xl font-bold`)}>{lastName}</Text>
-      </View>
+	return (
+		<View style={tw(`bg-white rounded-lg p-4 shadow-md`)}>
+			<View style={tw(`flex-row items-center mb-4`)}>
+				<Text style={tw(`text-2xl font-bold mr-2`)}>{firstName}</Text>
+				<Text style={tw(`text-2xl font-bold`)}>{lastName}</Text>
+			</View>
 
-      <View style={tw(`mb-4`)}>
-        <Text style={tw(`text-gray-500`)}>{jobTitle}</Text>
-        <Text style={tw(`text-gray-500`)}>{company}</Text>
-      </View>
+			<View style={tw(`mb-4`)}>
+				<Text style={tw(`text-gray-500`)}>{jobTitle}</Text>
+				<Text style={tw(`text-gray-500`)}>{company}</Text>
+			</View>
 
-      <View style={tw(`mb-4`)}>
-        <Text style={tw(`text-lg font-bold`)}>Phone Numbers:</Text>
-        {phoneNumbers.map((phoneNumber) => (
-          <Text key={phoneNumber.id} style={tw(`text-gray-500`)}>
-            {phoneNumber.label}: {phoneNumber.number}
-          </Text>
-        ))}
-      </View>
+			<View style={tw(`mb-4`)}>
+				<Text style={tw(`text-lg font-bold`)}>Phone Numbers:</Text>
+				{phoneNumbers.map((phoneNumber) => (
+					<TouchableOpacity
+						onPress={() =>
+							Communications.phonecall(String(phoneNumber.number), true)
+						}>
+						<Text key={phoneNumber.id} style={tw(`text-gray-500`)}>
+							{phoneNumber.label}: {phoneNumber.number}
+						</Text>
+					</TouchableOpacity>
+				))}
+			</View>
 
-      <View style={tw(`mb-4`)}>
-        <Text style={tw(`text-lg font-bold`)}>Emails:</Text>
-        {emails?.map((email) => (
-          <Text key={email.id} style={tw(`text-gray-500`)}>
-            {email.label}: {email.email}
-          </Text>
-        ))}
-      </View>
-    </View>
-  );
-};
+			<View style={tw(`mb-4`)}>
+				<Text style={tw(`text-lg font-bold`)}>Emails:</Text>
+				{emails?.map((email) => (
+					<Text key={email.id} style={tw(`text-gray-500`)}>
+						{email.label}: {email.email}
+					</Text>
+				))}
+			</View>
+		</View>
+	)
+}
 
-export default ContactCard;
+export default ContactCard
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,7 @@ import {
 	TouchableOpacity,
 	ScrollView,
 } from 'react-native'
-import tw  from 'tailwind-rn'
+import tw from 'tailwind-rn'
 import useAuth from '../hooks/useAuth'
 import { StatusBar } from 'expo-status-bar'
 import { useNavigation } from '@react-navigation/core'
@@ -27,20 +27,18 @@ import {
 } from '@firebase/firestore'
 import { db } from '../firebase'
 
-import Communications from 'react-native-communications'
-
 import * as Contacts from 'expo-contacts'
-import ContactCard from '../components/ContactCard';
+import ContactCard from '../components/ContactCard'
 
 function Home() {
 	const { user, logout } = useAuth()
 	const [profiles, setProfiles] = useState([])
 	const navigation = useNavigation()
 	const swipeRef = useRef(null)
-	
+
 	let [error, setError] = useState(undefined)
 	let [contacts, setContacts] = useState(undefined)
-		
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerShown: false,
@@ -72,60 +70,25 @@ function Home() {
 		})()
 	}, [])
 
-	// let ContactCard = (phone) => {
-	// 	console.log('phone', phone)
-	// 	return (
-	// 		<View style={tw('flex-1 relative')}>
-	// 			<Text style={styles.text}>Make phone call</Text>
-
-	// 			{/* Phone */}
-	// 			<TouchableOpacity
-	// 				onPress={() =>
-	// 					Communications.phonecall(String(phone.phone), true)
-	// 				}>
-	// 				<View style={styles.holder}>
-	// 					<Text style={styles.text}>Make phone call</Text>
-	// 				</View>
-	// 			</TouchableOpacity>
-
-	// 			{/* Text */}
-	// 			<TouchableOpacity
-	// 				onPress={() => Communications.text(phone.phone)}>
-	// 				<View style={styles.holder}>
-	// 					<Text style={styles.text}>Send a text/iMessage</Text>
-	// 				</View>
-	// 			</TouchableOpacity>
-	// 		</View>
-	// 	)
-	// }
-
-	// let getContactData = (data, property) => {
-	// 	if (data) {
-	// 		return data.map((data, index) => {
-	// 			return (
-	// 				<View key={index}>
-	// 					<Text>
-	// 						{data.label}: {data[property]}
-	// 					</Text>
-	// 				</View>
-	// 			)
-	// 		})
-	// 	}
-	// }
+	let getContactData = (data, property) => {
+		if (data) {
+			return data.map((data, index) => {
+				return (
+					<View key={index}>
+						<Text>
+							{data.label}: {data[property]}
+						</Text>
+					</View>
+				)
+			})
+		}
+	}
 
 	let getContactRows = () => {
-		console.log(contacts);
+		console.log(contacts)
 		if (contacts !== undefined) {
 			return contacts.map((contact, index) => {
-				return (
-					// <ContactCard
-					// 	key={index}
-					// 	phone={contact.phoneNumbers[0].digits}
-					// 	email={contact.email}
-					// 	web={contact.web}
-					// />
-					<ContactCard contact={contact} key={index}/>
-				)
+				return <ContactCard contact={contact} key={index} />
 			})
 		} else {
 			return <Text>Awaiting contacts...</Text>
@@ -161,23 +124,5 @@ function Home() {
 		</SafeAreaView>
 	)
 }
-
-const styles = StyleSheet.create({
-	contact: {
-		marginVertical: 8,
-	},
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		backgroundColor: 'rgb(253,253,253)',
-	},
-	holder: {
-		flex: 0.25,
-		justifyContent: 'center',
-	},
-	text: {
-		fontSize: 32,
-	},
-})
 
 export default Home
