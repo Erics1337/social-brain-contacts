aebb82201d235416ea7964a7200eb713eebfd3a8
Eric <erics1337@gmail.com>
2023-07-05 14:56:39 -0600

    Starter w/ Typescript & Tailwind

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,154 +1,22 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import {
-	Text,
-	View,
-	Image,
-	StyleSheet,
-	SafeAreaView,
-	TouchableOpacity,
-	ScrollView,
-	Picker,
-} from 'react-native'
-import tw from 'tailwind-rn'
-import useAuth from '../hooks/useAuth'
-import { StatusBar } from 'expo-status-bar'
-import { useNavigation } from '@react-navigation/core'
-import { AntDesign, Entypo, Ionicons } from '@expo/vector-icons'
-import {
-	collection,
-	doc,
-	getDoc,
-	getDocs,
-	onSnapshot,
-	orderBy,
-	query,
-	serverTimestamp,
-	setDoc,
-	where,
-} from '@firebase/firestore'
-import { db } from '../firebase'
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { signOut } from 'firebase/auth';
 
-import * as Contacts from 'expo-contacts'
-import ContactCard from '../components/ContactCard'
+import { auth } from '../config';
 
-import useStore from '../store'
+export const HomeScreen = () => {
+  const handleLogout = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
+  return (
+    <View style={styles.container}>
+      <Button title='Sign Out' onPress={handleLogout} />
+    </View>
+  );
+};
 
-function Home() {
-	const { user, logout } = useAuth()
-	const [profiles, setProfiles] = useState([])
-	const navigation = useNavigation()
-	const swipeRef = useRef(null)
-
-	let [error, setError] = useState(undefined)
-	// let [contacts, setContacts] = useState(undefined)
-
-	// const [selectedValue, setSelectedValue] = useState('family')
-	// const state = useStore()
-	const [setUser, binName, setBinName, contacts, setContacts] = useStore(
-		(state) => [
-			state.setUser,
-			state.binName,
-			state.setBinName,
-			state.contacts,
-			state.setContacts,
-		]
-	)
-
-	useLayoutEffect(() => {
-		navigation.setOptions({
-			headerShown: false,
-		})
-	}, [])
-
-	useEffect(() => {
-		;(async () => {
-			const { status } = await Contacts.requestPermissionsAsync()
-			if (status === 'granted') {
-				const { data } = await Contacts.getContactsAsync({
-					fields: [
-						Contacts.Fields.Birthday,
-						Contacts.Fields.Emails,
-						Contacts.Fields.FirstName,
-						Contacts.Fields.LastName,
-						Contacts.Fields.PhoneNumbers,
-					],
-				})
-
-				if (data.length > 0) {
-					// sync contact id's and bins with firestore
-					// Set contacts to state
-					setUser(user)
-					setContacts(data)
-				} else {
-					setError('No contacts found')
-				}
-			} else {
-				setError('Permission to access contacts denied.')
-			}
-		})()
-	}, [])
-
-	let getContactRows = () => {
-		if (contacts) {
-			return contacts.map((contact, index) => {
-				return <ContactCard contact={contact} key={index} />
-			})
-		} else {
-			return <Text>Awaiting contacts...</Text>
-		}
-	}
-
-	console.log('user', user)
-
-	return (
-		<SafeAreaView style={tw('flex-1 relative')}>
-			<View style={tw('items-center relative')}>
-				{user && (
-					<TouchableOpacity
-						onPress={logout}
-						style={tw('absolute left-5 top-3')}>
-						<Image
-							style={tw('h-10 w-10 rounded-full')}
-							source={{ uri: user.photoUrl }}
-						/>
-					</TouchableOpacity>
-				)}
-			</View>
-			<View style={tw('flex-1 -mt-6')}>
-				<View>
-					<Picker
-						selectedValue={binName}
-						style={{ height: 80 }}
-						onValueChange={(itemValue, itemIndex) =>
-							setBinName(itemValue)
-						}>
-						<Picker.Item label='Loved' value='loved' />
-						<Picker.Item label='Family' value='family' />
-						<Picker.Item label='Friends' value='friends' />
-						<Picker.Item
-							label='Acquaintances'
-							value='acquaintances'
-						/>
-						<Picker.Item
-							label='Recognizable'
-							value='recognizable'
-						/>
-					</Picker>
-				</View>
-				<View
-					style={tw(
-						'flex-1 bg-white mt-24 items-center justify-center'
-					)}>
-					<Text>{binName}</Text>
-					<ScrollView>{getContactRows()}</ScrollView>
-					<Text>{error}</Text>
-					<StatusBar style='auto' />
-				</View>
-			</View>
-
-			<StatusBar style='auto' />
-		</SafeAreaView>
-	)
-}
-
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,40 +1,122 @@
-import { useNavigation } from "@react-navigation/core";
-import React, { useLayoutEffect } from "react";
-import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
-import tw from "tailwind-rn";
-import useAuth from "../hooks/useAuth";
+import React, { useState } from 'react'
+import { Text, StyleSheet } from 'react-native'
+import { Formik } from 'formik'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-const LoginScreen = () => {
-  const { user, loading, error, signInWithGoogle, logout } = useAuth();
-  const navigation = useNavigation();
+import { View, TextInput, Logo, Button, FormErrorMessage } from '../components'
+import { Images, Colors, auth } from '../config'
+import { useTogglePasswordVisibility } from '../hooks'
+import { loginValidationSchema } from '../utils'
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
+export const LoginScreen = ({ navigation }) => {
+	const [errorState, setErrorState] = useState('')
+	const { passwordVisibility, handlePasswordVisibility, rightIcon } =
+		useTogglePasswordVisibility()
 
-  return (
-    <View style={tw("flex-1")}>
-      <ImageBackground
-        resizeMode="cover"
-        style={tw("flex-1")}
-        source={{ uri: "https://preview.redd.it/k35gae3somo41.png?auto=webp&s=07154e8f6b16a8480a5106f2cd62d3f12197f061" }}
-      >
-        <TouchableOpacity
-          onPress={signInWithGoogle}
-          style={[
-            tw("bg-white absolute bottom-40 w-52 rounded-2xl p-4"),
-            { marginHorizontal: "25%" },
-          ]}
-        >
-          <Text style={[tw("font-semibold text-center"), { color: "#FF5864" }]}>
-            Sign in
-          </Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </View>
-  );
-};
-
-export default LoginScreen;
+	const handleLogin = (values) => {
+		const { email, password } = values
+		signInWithEmailAndPassword(auth, email, password).catch((error) =>
+			setErrorState(error.message)
+		)
+	}
+	return (
+		<>
+			<View className='flex-1 bg-white'>
+				<KeyboardAwareScrollView enableOnAndroid={true}>
+					{/* LogoContainer: consists app logo and screen title */}
+					<View className='items-center'>
+						<Logo uri={Images.logo} />
+						<Text className='text-4xl font-bold text-black pt-5'>
+							Welcome back!
+						</Text>
+					</View>
+					<Formik
+						initialValues={{
+							email: '',
+							password: '',
+						}}
+						validationSchema={loginValidationSchema}
+						onSubmit={(values) => handleLogin(values)}>
+						{({
+							values,
+							touched,
+							errors,
+							handleChange,
+							handleSubmit,
+							handleBlur,
+						}) => (
+							<>
+								{/* Input fields */}
+								<TextInput
+									name='email'
+									leftIconName='email'
+									placeholder='Enter email'
+									autoCapitalize='none'
+									keyboardType='email-address'
+									textContentType='emailAddress'
+									autoFocus={true}
+									value={values.email}
+									onChangeText={handleChange('email')}
+									onBlur={handleBlur('email')}
+								/>
+								<FormErrorMessage
+									error={errors.email}
+									visible={touched.email}
+								/>
+								<TextInput
+									name='password'
+									leftIconName='key-variant'
+									placeholder='Enter password'
+									autoCapitalize='none'
+									autoCorrect={false}
+									secureTextEntry={passwordVisibility}
+									textContentType='password'
+									rightIcon={rightIcon}
+									handlePasswordVisibility={
+										handlePasswordVisibility
+									}
+									value={values.password}
+									onChangeText={handleChange('password')}
+									onBlur={handleBlur('password')}
+								/>
+								<FormErrorMessage
+									error={errors.password}
+									visible={touched.password}
+								/>
+								{/* Display Screen Error Messages */}
+								{errorState !== '' && (
+									<FormErrorMessage
+										error={errorState}
+										visible={true}
+									/>
+								)}
+								{/* Login button */}
+								<Button
+									className='w-full justify-center items-center mt-2 bg-orange-500 p-4 rounded'
+									onPress={handleSubmit}>
+									<Text className='text-white text-lg font-bold'>
+										Login
+									</Text>
+								</Button>
+							</>
+						)}
+					</Formik>
+					{/* Button to navigate to SignupScreen to create a new account */}
+					<Button
+						className='mt-4 items-center justify-center'
+						borderless
+						title={'Create a new account?'}
+						onPress={() => navigation.navigate('Signup')}
+					/>
+					<Button
+						className='mt-4 items-center justify-center'
+						borderless
+						title={'Forgot Password'}
+						onPress={() => navigation.navigate('ForgotPassword')}
+					/>
+				</KeyboardAwareScrollView>
+			</View>
+		</>
+	)
+}
