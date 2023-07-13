3fd755aa59af58d779f921f321d80cc07a907fac
Eric <erics1337@gmail.com>
2023-07-12 22:43:47 -0600

    rm Context API

--- App.tsx
+++ App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import RootNavigator from './navigation/RootNavigator';
-import { AuthenticatedUserProvider } from './providers';
 
 const App = () => {
   return (
-    <AuthenticatedUserProvider>
       <SafeAreaProvider>
         <RootNavigator />
       </SafeAreaProvider>
-    </AuthenticatedUserProvider>
   );
 };
 
--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,10 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { onAuthStateChanged } from 'firebase/auth'
 
 import AuthStack from './AuthStack'
 import AppStack from './AppStack'
-import { AuthenticatedUserContext } from '../providers'
 import { LoadingIndicator } from '../components'
 import { auth } from '../config'
 import { syncContacts } from '../services/contactService'
