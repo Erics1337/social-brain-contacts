40f18b0f552176c397dc19e619463d68b6e20e4c
Eric <erics1337@gmail.com>
2023-07-21 22:10:28 -0600

    No Contacts Message

--- components/ContactComponents/ContactList.tsx
+++ components/ContactComponents/ContactList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, SafeAreaView } from 'react-native'
+import { FlatList, SafeAreaView, Text } from 'react-native'
 import Contact from './Contact'
 import useStore from '../../store'
 import { LoadingIndicator } from '../LoadingIndicator'
@@ -16,6 +16,9 @@ const ContactList = () => {
 				data={filteredContacts}
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => <Contact contact={item} />}
+				ListEmptyComponent={()=>(
+					<Text className='flex-1 bg-gray-100 m-auto text-xl'>No Contacts</Text>
+				)}
 			/>
 		</SafeAreaView>
 	)
