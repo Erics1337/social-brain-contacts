95c7277cfb95e6358e86c79a2cbcc5caa3dcfc95
Eric <erics1337@gmail.com>
2023-07-30 15:13:25 -0600

    rm providers

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import Providers from './navigation';
 import Routes from './navigation/Routes';
 
 const App = () => {
