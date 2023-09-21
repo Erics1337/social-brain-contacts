ceb62e8ed9c0b6d9719d6cc6d97392ba5bbde314
Eric <erics1337@gmail.com>
2023-09-21 12:37:03 -0600

    Batch syncContacts

--- services/contactService.ts
+++ services/contactService.ts
@@ -20,58 +20,74 @@ export async function syncContacts(userId: string) {
 		const userContactsRef = collection(db, 'users', userId, 'contacts')
 		console.log('Syncing contacts to firestore')
 
+		// Write batch to handle updates
 		const batch = writeBatch(db)
+		let batchCount = 0
+
+		// Read all documents in a batch instead of one-by-one
+		const snapshot = await getDocs(userContactsRef)
+		const existingContacts = snapshot.docs.map((doc) => ({
+			id: doc.id,
+			bin: doc.data().bin,
+		}))
 
 		for (const contact of data) {
-			const contactRef = doc(userContactsRef, contact.id)
-			const currentDoc = await getDoc(contactRef)
+			// Find existing contact by id
+			const existingContact = existingContacts.find(
+				(c) => c.id === contact.id
+			)
 
 			// If 'bin' is not already present or is an empty string, set it to ''
-			if (!currentDoc.exists() || !currentDoc.data().bin) {
+			if (!existingContact || !existingContact.bin) {
+				const contactRef = doc(userContactsRef, contact.id)
 				batch.set(contactRef, { bin: '' }, { merge: true })
+				batchCount++
+
+				// Commit batch if it reaches the limit
+				if (batchCount >= 500) {
+					await batch.commit()
+					batchCount = 0
+				}
 			}
 		}
 
 		try {
-			await batch.commit()
+			if (batchCount > 0) {
+				await batch.commit()
+			}
 			console.log('All contacts updated in firebase successfully')
 		} catch (err) {
 			console.log('error syncing contacts to firebase', err)
 		}
 
-		try {
-			const snapshot = await getDocs(userContactsRef)
-
-			const firebaseIdsWithBins = snapshot.docs.map((doc) => ({
-				id: doc.id,
-				bin: doc.data().bin,
-			}))
-
-			const deleteBatch = writeBatch(db)
-
-			const phoneContactIds = data.map((c) => c.id)
-			snapshot.docs.forEach((doc) => {
-				if (!phoneContactIds.includes(doc.id)) {
-					deleteBatch.delete(doc.ref)
-				}
-			})
-
-			await deleteBatch.commit()
-
-			const enrichedContacts = data.map((phoneContact) => {
-				const associatedBin = firebaseIdsWithBins.find(
-					(fbContact) => fbContact.id === phoneContact.id
-				)?.bin
-				return {
-					...phoneContact,
-					bin: associatedBin || '',
-				}
-			})
-
-			useStore.getState().setContacts(enrichedContacts)
-			useStore.getState().setBinnedContacts()
-		} catch (error) {
-			console.log('error getting contacts from firebase', error)
+		// Delete batch to handle deletions
+		const deleteBatch = writeBatch(db)
+		const phoneContactIds = data.map((c) => c.id)
+		for (const existingContact of existingContacts) {
+			if (!phoneContactIds.includes(existingContact.id)) {
+				const contactRef = doc(userContactsRef, existingContact.id)
+				deleteBatch.delete(contactRef)
+			}
 		}
+
+		try {
+			await deleteBatch.commit()
+		} catch (error) {
+			console.log('error deleting contacts from firebase', error)
+		}
+
+		// Create enrichedContacts using a map for efficient lookups
+		const enrichedContacts = data.map((phoneContact) => {
+			const associatedBin = existingContacts.find(
+				(fbContact) => fbContact.id === phoneContact.id
+			)?.bin
+			return {
+				...phoneContact,
+				bin: associatedBin || '',
+			}
+		})
+
+		useStore.getState().setContacts(enrichedContacts)
+		useStore.getState().setBinnedContacts()
 	}
 }
