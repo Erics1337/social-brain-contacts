c0f78b07eb4bfbb44e7cfd833f435a6922b638a4
Eric <erics1337@gmail.com>
2023-11-18 21:38:48 -0700

    fix

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,11 +3,13 @@ import { View, Button, Text } from 'react-native'
 
 import ContactList from '../components/ContactComponents/ContactList'
 import CategoryPicker from '../components/CategoryPicker'
+import { SearchBox } from '../components/SearchBox'
 
 const HomeScreen: React.FC = () => {
 	return (
 		<View className='flex-1'>
 			<CategoryPicker />
+      <SearchBox />
 			<ContactList filterByBin={true} />
 		</View>
 	)
