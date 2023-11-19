82d542577cf04afe7be3c807a68e4152d6476eaa
Eric <erics1337@gmail.com>
2023-11-18 22:53:52 -0700

    Add menu to hide icons

--- components/ContactComponents/ActionIcons.tsx
+++ components/ContactComponents/ActionIcons.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { View, TouchableOpacity } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'
+import React from "react"
+import { View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 
 type ActionIconsProps = {
 	onCall: () => void
@@ -9,6 +9,8 @@ type ActionIconsProps = {
 	callDisabled: boolean
 	emailDisabled: boolean
 	textDisabled: boolean
+	toggleIcons: () => void
+	iconsExpanded: boolean
 }
 
 const ActionIcons: React.FC<ActionIconsProps> = ({
@@ -18,24 +20,49 @@ const ActionIcons: React.FC<ActionIconsProps> = ({
 	callDisabled,
 	emailDisabled,
 	textDisabled,
+	toggleIcons,
+	iconsExpanded,
 }) => {
 	return (
-		<View className='flex flex-row'>
-			{!callDisabled && (
-				<TouchableOpacity onPress={onCall}>
-					<MaterialIcons name='call' size={24} color='black' />
-				</TouchableOpacity>
+		<View className="flex-row items-center">
+			{iconsExpanded && (
+				<>
+					{!callDisabled && (
+						<TouchableOpacity
+							onPress={onCall}
+							className="p-1 m-1 rounded-full border border-gray-300"
+							activeOpacity={0.7}
+						>
+							<MaterialIcons name="call" size={24} color="black" />
+						</TouchableOpacity>
+					)}
+					{!textDisabled && (
+						<TouchableOpacity
+							onPress={onText}
+							className="p-1 m-1 rounded-full border border-gray-300"
+							activeOpacity={0.7}
+						>
+							<MaterialIcons name="message" size={24} color="black" />
+						</TouchableOpacity>
+					)}
+					{!emailDisabled && (
+						<TouchableOpacity
+							onPress={onEmail}
+							className="p-1 m-1 rounded-full border border-gray-300"
+							activeOpacity={0.7}
+						>
+							<MaterialIcons name="email" size={24} color="black" />
+						</TouchableOpacity>
+					)}
+				</>
 			)}
-			{!textDisabled && (
-				<TouchableOpacity onPress={onText}>
-					<MaterialIcons name='message' size={24} color='black' />
-				</TouchableOpacity>
-			)}
-			{!emailDisabled && (
-				<TouchableOpacity onPress={onEmail}>
-					<MaterialIcons name='email' size={24} color='black' />
-				</TouchableOpacity>
-			)}
+			<TouchableOpacity
+				onPress={toggleIcons}
+				className="p-1 m-1 rounded border border-gray-300"
+				activeOpacity={0.7}
+			>
+				<MaterialIcons name="menu" size={24} color="black" />
+			</TouchableOpacity>
 		</View>
 	)
 }
--- components/ContactComponents/Contact.tsx
+++ components/ContactComponents/Contact.tsx
@@ -1,80 +1,66 @@
-import React, { useState, useEffect } from 'react'
-import {
-	Text,
-	View,
-	TouchableWithoutFeedback,
-	Modal,
-	TouchableOpacity,
-	Pressable,
-} from 'react-native'
-import Avatar from './Avatar'
-import ActionIcons from './ActionIcons'
-import { OverloadedExpoContact } from '../../types'
-import { openURL } from 'expo-linking'
-import { CategoriesModal } from './CategoriesModal'
+import React, { useState } from "react"
+import { Text, View, TouchableOpacity } from "react-native"
+import Avatar from "./Avatar"
+import ActionIcons from "./ActionIcons"
+import { OverloadedExpoContact } from "../../types"
+import { openURL } from "expo-linking"
+import { CategoriesModal } from "./CategoriesModal"
 
 type ContactProps = {
 	contact: OverloadedExpoContact
 }
 
 const Contact = React.memo(({ contact }: ContactProps) => {
-	const firstName = contact.firstName ?? ''
-	const lastName = contact.lastName ?? ''
+	const firstName = contact.firstName ?? ""
+	const lastName = contact.lastName ?? ""
 
 	const getFullName = (firstName?: string, lastName?: string) => {
-		// Filter out undefined or empty strings and join the remaining with a space
-		return [firstName, lastName].filter(Boolean).join(' ')
+		return [firstName, lastName].filter(Boolean).join(" ")
 	}
 
 	const fullName = getFullName(contact.firstName, contact.lastName)
 
 	const getInitials = (firstName?: string, lastName?: string) => {
-		let initials = ''
-
+		let initials = ""
 		if (firstName && firstName.length > 0) {
 			initials += firstName[0].toUpperCase()
 		}
-
 		if (lastName && lastName.length > 0) {
 			initials += lastName[0].toUpperCase()
 		}
-
 		return initials
 	}
 
 	const initials = getInitials(firstName, lastName)
 
 	const [isModalVisible, setIsModalVisible] = useState(false)
+	const [iconsExpanded, setIconsExpanded] = useState(false)
 
 	// Handlers
 	const handleCall = () => {
 		if (contact?.phoneNumbers?.[0]?.number) {
-			openURL(`tel://+1${contact.phoneNumbers[0].number}`).catch(
-				(error) => {
-					console.log(error)
-				}
-			)
+			openURL(`tel://+1${contact.phoneNumbers[0].number}`).catch((error) => {
+				console.log(error)
+			})
 		}
 	}
 
 	const handleText = () => {
 		if (contact?.phoneNumbers?.[0]?.number) {
-			openURL(`sms://+1${contact.phoneNumbers[0].number}`).catch(
-				(error) => {
-					console.log(error)
-				}
-			)
+			openURL(`sms://+1${contact.phoneNumbers[0].number}`).catch((error) => {
+				console.log(error)
+			})
 		}
 	}
 
 	const handleEmail = () => {
 		if (contact?.emails?.[0]?.email) {
 			const email = contact.emails[0].email
-			console.log('Email:', email) // Log the email
+			console.log("Email:", email) // Log the email
 			openURL(`mailto:${email}`).catch((error) => {
 				console.log(error)
 			})
-		} else console.log('no contact email found')
+		} else console.log("no contact email found")
 	}
 
 	const handleLongPress = () => {
@@ -85,12 +71,14 @@ const Contact = React.memo(({ contact }: ContactProps) => {
 		setIsModalVisible(false)
 	}
 
+	const toggleIcons = () => setIconsExpanded(!iconsExpanded)
+
 	return (
 		<TouchableOpacity activeOpacity={0.5} onLongPress={handleLongPress}>
-			<View className='flex flex-row items-center justify-between p-4 border-b border-gray-200'>
+			<View className="flex flex-row items-center justify-between p-4 border-b border-gray-200">
 				<Avatar initials={initials} />
-				<View className='flex-1 mx-3'>
-					<Text className='text-lg font-bold ml-4'>{fullName}</Text>
+				<View className={`flex-1 mx-3 ${iconsExpanded ? "mr-2" : ""}`}>
+					<Text className="text-lg font-bold ml-4">{fullName}</Text>
 				</View>
 				<ActionIcons
 					onCall={handleCall}
@@ -99,12 +87,11 @@ const Contact = React.memo(({ contact }: ContactProps) => {
 					callDisabled={false}
 					emailDisabled={!contact?.emails?.length}
 					textDisabled={false}
+					toggleIcons={toggleIcons}
+					iconsExpanded={iconsExpanded}
 				/>
 				{isModalVisible && (
-					<CategoriesModal
-						contact={contact}
-						onClose={handleCloseModal}
-					/>
+					<CategoriesModal contact={contact} onClose={handleCloseModal} />
 				)}
 			</View>
 		</TouchableOpacity>
