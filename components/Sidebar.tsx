b627c131f3cc98db40cb4db40e70c6495e0189cc
Eric <erics1337@gmail.com>
2023-11-18 22:54:20 -0700

    Menu refactor

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { View, Text, TouchableOpacity, Animated, Button } from 'react-native'
-import useStore from '../store'
-import { signOut } from 'firebase/auth'
-import { auth } from '../config'
-import DeleteUserModal from './DeleteUserModal'
-import { useNavigation } from '@react-navigation/native'
-import * as Application from 'expo-application'
-import { Linking } from 'react-native'
+import React, { useEffect, useRef, useState } from "react"
+import { View, Text, TouchableOpacity, Animated, Button } from "react-native"
+import useStore from "../store"
+import { signOut } from "firebase/auth"
+import { auth } from "../config"
+import DeleteUserModal from "./DeleteUserModal"
+import { useNavigation } from "@react-navigation/native"
+import * as Application from "expo-application"
+import { Linking } from "react-native"
 
 const Sidebar: React.FC = () => {
 	const navigation = useNavigation()
@@ -28,7 +28,7 @@ const Sidebar: React.FC = () => {
 		try {
 			signOut(auth)
 		} catch (error) {
-			console.log('Error logging out: ', error)
+			console.log("Error logging out: ", error)
 		}
 	}
 
@@ -65,45 +65,42 @@ const Sidebar: React.FC = () => {
 			{/* Main Sidebar */}
 			<Animated.View
 				style={{
-					position: 'absolute',
+					position: "absolute",
 					right: slideAnim,
 					top: 50,
 					bottom: 0,
-					width: '60%',
-					backgroundColor: '#fff',
+					width: "60%",
+					backgroundColor: "#fff",
 					padding: 10,
 					zIndex: 1,
-				}}>
-				<TouchableOpacity onPress={toggleSidebar}>
+				}}
+			>
+				<TouchableOpacity onPress={toggleSidebar} className='mb-5'>
 					<Text style={{ fontSize: 24 }}>X</Text>
 				</TouchableOpacity>
-				<View style={{ flexDirection: 'column' }}>
+				<View style={{ flexDirection: "column" }}>
 					<TouchableOpacity
-						className='bg-primary py-2 px-4 rounded'
+						className="bg-primary py-2 px-4 rounded mb-3"
 						onPress={() => {
-							navigation.navigate('Sort')
+							navigation.navigate("Sort")
 							toggleSidebar()
-						}}>
-						<Text className='text-white text-center text-xl'>
+						}}
+					>
+						<Text className="text-white text-center text-xl">
 							Sort Contacts
 						</Text>
 					</TouchableOpacity>
 					<TouchableOpacity
-						style={{ marginBottom: 10 }}
-						onPress={() => setShowAccountSidebar(true)}>
-						<Text className='text-primary text-center text-xl'>
-							Account
-						</Text>
+						className='mb-5'
+						onPress={() => setShowAccountSidebar(true)}
+					>
+						<Text className="text-primary text-center text-xl">Account</Text>
 					</TouchableOpacity>
 					<TouchableOpacity
-						style={{ marginBottom: 10 }}
-						onPress={() => setShowAboutSidebar(true)}>
-						<Text className='text-primary text-center text-xl'>About</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={{ marginBottom: 10 }}
-						onPress={() => handleLogout()}>
-						<Text className='text-center text-xl'>Sign Out</Text>
+						className='mb-5'
+						onPress={() => setShowAboutSidebar(true)}
+					>
+						<Text className="text-primary text-center text-xl">About</Text>
 					</TouchableOpacity>
 				</View>
 			</Animated.View>
@@ -111,62 +108,70 @@ const Sidebar: React.FC = () => {
 			{/* Account Sidebar */}
 			<Animated.View
 				style={{
-					position: 'absolute',
+					position: "absolute",
 					right: accountSlideAnim,
 					top: 50,
 					bottom: 0,
-					width: '60%',
-					backgroundColor: '#f5f5f5',
+					width: "60%",
+					backgroundColor: "#f5f5f5",
 					padding: 10,
 					zIndex: 2,
-				}}>
+				}}
+			>
 				<TouchableOpacity onPress={() => setShowAccountSidebar(false)}>
 					<Text style={{ fontSize: 24 }}>&larr;</Text>
 				</TouchableOpacity>
-				<View style={{ flexDirection: 'column' }}>
-					<Button
-						title='Delete Account'
+				<View style={{ flexDirection: "column" }}>
+					<TouchableOpacity
+						className='mb-2'
 						onPress={() => toggleAccountDeleteModal()}
-					/>
-					{/* <Button title='Other Option' onPress={() => console.log('Other Option')} /> */}
+					>
+						<Text className="text-center text-xl text-red">Delete Account</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={{ marginBottom: 10 }}
+						onPress={() => handleLogout()}
+					>
+						<Text className="text-center text-xl text-blue-300">Sign Out</Text>
+					</TouchableOpacity>
 				</View>
 			</Animated.View>
 
 			{/* About Sidebar */}
 			<Animated.View
 				style={{
-					position: 'absolute',
+					position: "absolute",
 					right: aboutSlideAnim,
 					top: 50,
 					bottom: 0,
-					width: '60%',
-					backgroundColor: '#f5f5f5',
+					width: "60%",
+					backgroundColor: "#f5f5f5",
 					padding: 10,
 					zIndex: 2,
-				}}>
+				}}
+			>
 				<TouchableOpacity onPress={() => setShowAboutSidebar(false)}>
-					<Text className='text-2xl'>←</Text>
+					<Text className="text-2xl">←</Text>
 				</TouchableOpacity>
-				<View className='flex-col'>
-					<Text className='text-lg font-medium mb-1'>
+				<View className="flex-col">
+					<Text className="text-lg font-medium mb-1">
 						Social Brain Contacts
 					</Text>
-					<Text className='text-base mb-1'>
+					<Text className="text-base mb-1">
 						App Name: {Application.applicationName}
 					</Text>
-					<Text className='text-base mb-1'>
+					<Text className="text-base mb-1">
 						App Version: {Application.nativeApplicationVersion}
 					</Text>
-					<Text className='text-base mb-1'>
+					<Text className="text-base mb-1">
 						Build Version: {Application.nativeBuildVersion}
 					</Text>
 					<TouchableOpacity
 						onPress={() =>
-							Linking.openURL(
-								'https://www.socialbraincontacts.com'
-							)
-						}>
-						<Text className='text-base text-blue-500 underline'>
+							Linking.openURL("https://www.socialbraincontacts.com")
+						}
+					>
+						<Text className="text-base text-blue-500 underline">
 							Visit Website
 						</Text>
 					</TouchableOpacity>
