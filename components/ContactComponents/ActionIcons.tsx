04178784bf11e9e100beacf3c91eaead9bdbe95e
Eric <erics1337@gmail.com>
2023-11-18 22:55:42 -0700

    Icons Adjustment

--- components/ContactComponents/ActionIcons.tsx
+++ components/ContactComponents/ActionIcons.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { View, TouchableOpacity } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'
+import React from "react"
+import { View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 
 type ActionIconsProps = {
 	onCall: () => void
@@ -20,20 +20,32 @@ const ActionIcons: React.FC<ActionIconsProps> = ({
 	textDisabled,
 }) => {
 	return (
-		<View className='flex flex-row'>
+		<View className="flex-row items-center justify-evenly">
 			{!callDisabled && (
-				<TouchableOpacity onPress={onCall}>
-					<MaterialIcons name='call' size={24} color='black' />
+				<TouchableOpacity
+					onPress={onCall}
+					className="p-1 m-1 rounded border border-gray-300" // Reduced padding and added border
+					activeOpacity={0.7}
+				>
+					<MaterialIcons name="call" size={24} color="black" />
 				</TouchableOpacity>
 			)}
 			{!textDisabled && (
-				<TouchableOpacity onPress={onText}>
-					<MaterialIcons name='message' size={24} color='black' />
+				<TouchableOpacity
+					onPress={onText}
+					className="p-1 m-1 rounded border border-gray-300"
+					activeOpacity={0.7}
+				>
+					<MaterialIcons name="message" size={24} color="black" />
 				</TouchableOpacity>
 			)}
 			{!emailDisabled && (
-				<TouchableOpacity onPress={onEmail}>
-					<MaterialIcons name='email' size={24} color='black' />
+				<TouchableOpacity
+					onPress={onEmail}
+					className="p-1 m-1 rounded border border-gray-300"
+					activeOpacity={0.7}
+				>
+					<MaterialIcons name="email" size={24} color="black" />
 				</TouchableOpacity>
 			)}
 		</View>
